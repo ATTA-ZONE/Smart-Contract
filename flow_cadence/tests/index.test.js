c56d093da7d967786b13b363ec803cafa8897505
Rangerx42 <rangerx42@gmail.com>
2021-11-17 14:37:18 +0800

    chore: update test case

--- flow_cadence/tests/index.test.js
+++ flow_cadence/tests/index.test.js
@@ -1,7 +1,7 @@
 import { adminTest } from './testsuite/setup.js'
 import { useCase } from './testsuite/useCase.js'
 
-describe('flowns test case', () => {
+describe('contract test case', () => {
   adminTest()
   useCase()
 })
